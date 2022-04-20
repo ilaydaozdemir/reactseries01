4ebb8480d7e5817a7d236c6fee734cbb7fcfbfeb
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-20 18:43:53 +0300

    Add click event

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 const Content = () => {
-  //add jsx
-  const name = "ilayda";
-
   //add arrow function with jsx
   const handleNameChange = () => {
     const names = ["Bob", "Kevin", "Jack"];
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  //add click event
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
     <main>
       <p>Hello {handleNameChange()}</p>
-      <p>{name}</p>
+      <button onClick={handleClick}>Click it</button>
     </main>
   );
 };
