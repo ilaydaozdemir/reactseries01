2e2a6e522f9b42a6778be5105a3e9a60f6da76cd
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-20 16:58:16 +0300

    Add arrow function with jsx

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   //add jsx 
-  const name="ilayda"
+  const name = "ilayda"
+
+  //add arrow function with jsx
+  const handleNameChange = () => {
+    const names = ["Bob", "Kevin", "Jack"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int]
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {handleNameChange()}</p>
         <p>{name}</p>
       </header>
     </div>
