36c4754464b8a1f972a92194993cd297664be3fb
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-20 18:35:00 +0300

    Delete unnecessary things

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
 const Header = () => {
-  const headerStyle = {
-    backgroundColor: "royalblue",
-    color: "#fff",
-  };
   return (
     <header>
-      <h1 style={headerStyle}>Grocies List</h1>
+      <h1>Grocies List</h1>
     </header>
   );
 };
