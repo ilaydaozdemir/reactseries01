837be03ddcd5683bce7d9584b5c90d93ea3c917c
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-20 17:41:35 +0300

    Add style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <header>
-      <h1>Grocies List</h1>
-  </header>;
+  const headerStyle = {
+    backgroundColor: "royalblue",
+    color: "#fff",
+  };
+  return (
+    <header>
+      <h1 style={headerStyle}>Grocies List</h1>
+    </header>
+  );
 };
 
 export default Header;
