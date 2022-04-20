5b6c4434163c8bbf961080641316c89d85a168dc
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-20 19:07:03 +0300

    Some click events examples

--- src/Content.jsx
+++ src/Content.jsx
@@ -17,11 +17,16 @@ const Content = () => {
   const handleClick2 = (name) => {
     console.log(`${name} was clicked`);
   };
+  //add click event e
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
       <button onClick={handleClick}>Click it</button>
       <button onClick={() => handleClick2("ilayda")}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   );
 };
