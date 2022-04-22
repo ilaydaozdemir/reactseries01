ff029e29604eb8cb92171235f5915239ff402edb
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-22 15:59:28 +0300

    How to use defaultProps

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Header = (props) => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </header>
   );
+};
+Header.defaultProps = {
+  title: "Default Title",
 };
 
 export default Header;
