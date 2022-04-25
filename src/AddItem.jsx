7ad2058080ac92cd7b207c40a69243033321127c
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-25 14:13:34 +0300

    the useRef hook

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useRef } from "react";
 
 //add icon
 import { FaPlus } from "react-icons/fa";
 
 const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef();
+
   return (
     <form className="addForm" onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
+        ref={inputRef}
         id="addItem"
         type="text"
         placeholder="Add Item"
@@ -16,7 +20,11 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
         <FaPlus />
       </button>
     </form>
