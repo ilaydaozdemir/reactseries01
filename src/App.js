5452fb1ab92e4914788d477f71327e0bc11c6cbe
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-04-25 20:06:02 +0300

    saving data with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddItem from './AddItem';
 import SeachItem from './SeachItem';
 
@@ -14,27 +14,28 @@ function App() {
   //add search
   const [search, setSearch] = useState('')
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems)
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  }
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items));
+  }, [items])
+
+
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item }
     const listItems = [...items, myNewItem]
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
